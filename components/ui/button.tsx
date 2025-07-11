e6a2c97698c2640ea593964885b33e51c3066c76
HimanshiYad06 <himanshi.yadav.2206@gmail.com>
2025-07-11 22:59:19 +0530

    nav bar

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-[rgba(162,89,255,0.18)] hover:text-fuchsia-300 border border-transparent hover:border-fuchsia-400",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
